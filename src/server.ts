89aaedb7fbc509bc342a8ae6c7b6a32f48e96111
Henry Gabriel <hnrtk@hotmail.com>
2021-08-01 01:31:02 -0300

    aula-2

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
+import "reflect-metadata";
 import express from "express";
+
+import { router } from "./routes";
+
+import "./database";
 
 const app = express();
 
-app.get("/test", (req, res) => res.send("Olá Henry"));
-
-app.post("/test-post", (req, res) => res.send("Olá Post"));
+app.use(express.json());
+app.use(router);
 
 app.listen(3000, () => {
   console.log("Server is running...");
